0f74fef212194ade16af646095f9b9cb2caaee6a
willieochoa <willieochoa+ga@gmail.com>
2022-05-24 04:45:11 -0700

    implemented modals and timer for tools - GAME IS FULLY FUNCTIONAL

--- script.js
+++ script.js
@@ -9,6 +9,13 @@ const upgradeToolDisplay = document.getElementById("upgrade-tool")
 const moneyDisplay = document.getElementById("money-display")
 const toolImg = document.getElementById("tool-img")
 const grassImg = document.getElementsByClassName("grass")
+const waitTimer = document.getElementById("timer")
+
+//HTML FETCHING FOR UI INTERACTION
+const close = document.getElementById('close')
+const restart = document.getElementById('restart')
+const welcomeModal = document.getElementById('welcome-modal')
+const victoryModal = document.getElementById('victory-modal')
 
 
 //Setting up the game
@@ -21,19 +28,21 @@ let upgradeCounter=1;
 const toolNames = ["Teeth","Rusty Scissors","Push Lawnmower","Battery-Powered Lawnmower","Team of starving students"]
 const toolProfit = [1,5,50,100,250]
 const toolCost = [0,5,25,250,500]
+const toolDelay = [6,4,2,1,0.5]
 const toolImgs = ["./assets/tool-teeth.png","./assets/tool-shears.png","./assets/tool-manual-lawn-mower.png","./assets/tool-bat-lawnmower.png","./assets/tool-team.png"]
 
 class Tool{
-   constructor(name,profit,cost,img){
+   constructor(name,profit,cost,delay,img){
        this.name = name;
        this.profit = profit;
        this.cost = cost;
+       this.delay = delay;
        this.img = img;
    } 
 }
 
 for(let i = 0; i<toolNames.length; i++){
-    tools.push(new Tool(toolNames[i],toolProfit[i],toolCost[i],toolImgs[i]))
+    tools.push(new Tool(toolNames[i],toolProfit[i],toolCost[i],toolDelay[i],toolImgs[i]))
 }
 
 let currentTool=tools[toolIndex]
@@ -50,6 +59,7 @@ for(let i=0; i<grassImg.length; i++){
 //game variables
 
 let money = 0;
+moneyDisplay.innerHTML = `$${money}`;
 
 //let currentTool =tools[upgradeCounter];
 
@@ -58,10 +68,13 @@ let money = 0;
 //Button actions
 function cutGrassGrabCash(tool){
     tool = currentTool
-    money += tool.profit
-    moneyDisplay.innerHTML = money;
+    // money += tool.profit
+    // moneyDisplay.innerHTML = money;
     // console.log(money)
     cutGrassGrowGrass()
+    waitForTool(tool)
+    
+    
     }
     
     function buyTool(tool,upgradeTool){
@@ -71,13 +84,26 @@ function cutGrassGrabCash(tool){
             toolIndex += 1
             upgradeCounter += 1 
             toolImg.src=tools[toolIndex].img
+            moneyDisplay.innerHTML = `$${money}`
+            // currentTool=tools[toolIndex]
+            // upgradeTool =tools[upgradeCounter]
             //console.log(upgradeTool.cost)
 
         }
     }
     
+    const closeWelcomeModal = () => {welcomeModal.style.display = 'none'}
+    const reload = () => {welcomeModal.style.display = 'none'; location.reload()}
+    //yeah yeah. I know. DRY, but It'3 am, I overdid the whole thing and I feel like I can slip here and be fine
+    // const closeVictoryModal = () => {welcomeModal.style.display = 'none'}
+
     cutButton.addEventListener("click",()=>cutGrassGrabCash(currentTool))
     upgradeButton.addEventListener("click",()=>buyTool(currentTool,upgradeTool))
+
+    close.addEventListener('click', closeWelcomeModal)
+    restart.addEventListener('click', reload)
+    
+    
 
 
 
@@ -85,11 +111,12 @@ function cutGrassGrabCash(tool){
 function updateScreen(){
     currentTool=tools[toolIndex]
     upgradeTool =tools[upgradeCounter]
-    moneyDisplay.innerHTML = money;
+    // if(moneyDisplay.innerHTML != `$${money}`)
+    // moneyDisplay.innerHTML = `$${money}`;
     // console.log(toolIndex)
     // console.log(tools.length)
     if(toolIndex<tools.length-1){
-    upgradeToolDisplay.innerHTML = `Buy ${upgradeTool.name} for ${upgradeTool.cost}` 
+    upgradeToolDisplay.innerHTML = `Buy ${upgradeTool.name} for $${upgradeTool.cost}` 
 
         if(money<upgradeTool.cost || toolIndex>tools.length){
             upgradeButton.disabled = true
@@ -101,6 +128,10 @@ function updateScreen(){
         upgradeButton.disabled = true
         upgradeToolDisplay.innerHTML = `all upgrades purchased`
     }
+    if(money>=1000 && toolIndex>=tools.length-1){
+        victoryModal.style.display ='block'
+
+    }
     
 
 
@@ -109,6 +140,18 @@ function updateScreen(){
 setInterval(updateScreen,100)
 
 //growing grass
+
+function waitForTool(tool){
+    cutButton.disabled=true
+    
+    t = currentTool.delay*1000
+    waitTimer.innerHTML = `wating for ${currentTool.delay}s...`
+    setTimeout(function(){cutButton.disabled = false;waitTimer.innerHTML = "";money += tool.profit
+    moneyDisplay.innerHTML = money;},t);
+
+
+}
+
 function cutGrassGrowGrass(){
     isGrowingyooo= false
     console.log("grass should grow")
